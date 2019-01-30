7737f1a8607674d86dd27aec66a75339982ec1a2
washie <washie@gmail.com>
2019-01-30 21:34:54 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -334,7 +334,7 @@
 			      },
 			      error: function() {
 			      	sLoader.fadeOut(); 
-			      	$('#message-warning').html("Something went wrong. Please try again.");
+			      	$('#message-warning').html("Alguma coisa deu errado. Por favor, tente novamente.");
 			         $('#message-warning').fadeIn();
 			      }
 		      });    		
@@ -366,12 +366,12 @@
 
 		$.ajaxChimp.translations.es = {
 		  'submit': 'Submitting...',
-		  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-		  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-		  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+		  0: '<i class="fa fa-check"></i> Enviaremos um e-mail de confirmação.',
+		  1: '<i class="fa fa-warning"></i> Você precisa inserir um e-mail válido.',
+		  2: '<i class="fa fa-warning"></i> e-mail inválido.',
+		  3: '<i class="fa fa-warning"></i> e-mail inválido.',
+		  4: '<i class="fa fa-warning"></i> e-mail inválido.',
+		  5: '<i class="fa fa-warning"></i> e-mail inválido.'
 		} 
 
 	};
